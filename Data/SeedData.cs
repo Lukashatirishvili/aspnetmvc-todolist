702a0de0d8aa91f449e834610bdd54d3f8907dd7
Luka Shatirishvili <lukkashatirishvili@gmail.com>
2026-05-23 18:28:07 +0400

    Seed Data

---
+++ Data/SeedData.cs
@@ -0,0 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using TodolistWebApp.Models;
+
+namespace TodolistWebApp.Data;
+
+public class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using var context = new TodolistDbContext(
+            serviceProvider.GetRequiredService<DbContextOptions<TodolistDbContext>>());
+        if (context.Todos.Any())
+        {
+            return;
+        }
+
+        context.Todos.AddRange(
+            new Todo
+            {
+                TodoText = "Workout",
+                IsCompleted = false,
+            },
+            new Todo
+            {
+                TodoText = "Breakfast",
+                IsCompleted = false,
+            },
+            new Todo
+            {
+                TodoText = "Study",
+                IsCompleted = false,
+            },
+            new Todo
+            {
+                TodoText = "Reading",
+                IsCompleted = false,
+            }
+        );
+        context.SaveChanges();
+    }
+}
